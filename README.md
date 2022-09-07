70c97e9c6745dc34f0f9c9936cbc362dfc6a1b91
Envel Le Hir <envel@lehir.net>
2022-09-07 13:31:40 +0200

    cleaning and documentation

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 ## Description
 
-The script `retrieve-votes.py` checks if voters of the 2022 Wikimedia Foundation board elections have the bot flag.
+The script `retrieve-votes.py` checks that voters of the 2022 Wikimedia Foundation board elections comply with some of the [rules for voting eligibility](https://meta.wikimedia.org/wiki/Wikimedia_Foundation_elections/2022/Voter_eligibility_guidelines).
 
 The script `generate-statistics.py` generates various aggregated statistics about voters. It must be run after `retrieve-votes.py`.
+
+## Run
+
+This project requires Python 3.
+
+After downloading or cloning the project with your favorite git client, run the following command:
+
+    python3 src/retrieve-votes.py
+
+Optionally, you can generate statistics:
+
+    python3 src/generate-statistics.py
 
 ## Copyright
 
--- src/retrieve-votes.py
+++ src/retrieve-votes.py
@@ -39,36 +39,34 @@ def main():
         # votes data
         vote_matches = re.findall('<tr(?: class="([^<>]+)")?>\n<td class="TablePager_col_vote_id">([^<]+)</td>\n<td class="TablePager_col_vote_voter_name">([^<]+)</td>\n<td class="TablePager_col_vote_voter_domain">([^<]+)</td>\n</tr>', vote_data)
         for vote_match in vote_matches:
-            vote = {'login': html.unescape(vote_match[2]), 'home': vote_match[3], 'vote_date': vote_match[1], 'options': sorted(set(vote_match[0].split())), 'wikis': []}
+            vote = {'login': html.unescape(vote_match[2]), 'home': vote_match[3], 'vote_date': vote_match[1], 'options': sorted(set(vote_match[0].split())), 'wikis': [], 'registration': 'N/A'}
             if not vote['options']:
                 active_votes_count += 1
                 if vote['login'] in items_index:
                     items[items_index[vote['login']]]['votes'].add(vote['login'])
                     if items[items_index[vote['login']]]['death'] is not None:
                         print('According to Wikidata, {} is supposed to be dead.'.format(vote['login']))
-            wikis = []
-            wiki_data = utils.fetch_url('https://meta.wikimedia.org/wiki/Special:CentralAuth/{}'.format(vote['login'].replace(' ', '_')))
-            # year of registration
-            match = re.findall('<strong>Registered:</strong> [^(]+ ([0-9]{4}) \\(', wiki_data)
-            if match:
-                vote['registration'] = match.pop()
-            else:
-                vote['registration'] = 'N/A'
-            # wikis
-            wiki_matches = re.findall('<tr><td><a[^<>]+>([^<>]+)</a></td><td data-sort-value="([0-9]+)">[^<>]+</td><td style="text-align: center;"><img[^<>]+/><span[^<>]+>\\(\\?\\)</span></td><td><a[^<>]+>.*?</a></td><td style="text-align: right;"><a[^<>]+>([0-9,]+)</a></td><td>(.*?)</td></tr>', wiki_data)
-            for wiki_match in wiki_matches:
-                wiki = {'url': wiki_match[0], 'sub_date': wiki_match[1], 'edits': int(wiki_match[2].replace(',', '')), 'groups': sorted(set(filter(None, wiki_match[3].split(', '))))}
-                wikis.append(wiki)
-                if 'bot' in wiki['groups'] or 'copyviobot' in wiki['groups']:
-                    print('{} is in a bot group on {}'.format(vote['login'], wiki['url']))
-            vote['wikis'] = wikis
+                wikis = []
+                wiki_data = utils.fetch_url('https://meta.wikimedia.org/wiki/Special:CentralAuth/{}'.format(vote['login'].replace(' ', '_')))
+                # year of registration
+                match = re.findall('<strong>Registered:</strong> [^(]+ ([0-9]{4}) \\(', wiki_data)
+                if match:
+                    vote['registration'] = match.pop()
+                # wikis
+                wiki_matches = re.findall('<tr><td><a[^<>]+>([^<>]+)</a></td><td data-sort-value="([0-9]+)">[^<>]+</td><td style="text-align: center;"><img[^<>]+/><span[^<>]+>\\(\\?\\)</span></td><td><a[^<>]+>.*?</a></td><td style="text-align: right;"><a[^<>]+>([0-9,]+)</a></td><td>(.*?)</td></tr>', wiki_data)
+                for wiki_match in wiki_matches:
+                    wiki = {'url': wiki_match[0], 'sub_date': wiki_match[1], 'edits': int(wiki_match[2].replace(',', '')), 'groups': sorted(set(filter(None, wiki_match[3].split(', '))))}
+                    wikis.append(wiki)
+                    if 'bot' in wiki['groups'] or 'copyviobot' in wiki['groups']:
+                        print('{} is in a bot group on {}'.format(vote['login'], wiki['url']))
+                vote['wikis'] = wikis
             votes.append(vote)
     utils.file_put_contents('data/votes.json', json.dumps(votes))
     print('{} votes, {} active.'.format(len(votes), active_votes_count))
 
     for qid in items:
         if len(items[qid]['votes']) > 1:
-            print('Multiple votes by {} with accounts: {}'.format(qid, ', '.join(items[qid]['votes'])))
+            print('Multiple votes by {} with accounts: {}'.format(qid, ', '.join(sorted(items[qid]['votes']))))
 
 
 if __name__ == '__main__':
